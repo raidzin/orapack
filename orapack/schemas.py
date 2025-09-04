23fc0f0730a3c5048c49ce4b2dc37e7c9777899d
raidzin <agonch2001@gmail.com>
2025-09-04 23:08:03 +0300

    fixes

--- orapack/schemas.py
+++ orapack/schemas.py
@@ -3,10 +3,24 @@ from dataclasses import dataclass
 
 @dataclass
 class Argument:
+    """Used for function params or return value."""
+
     name: str
     type: str
 
 
 @dataclass
 class Function:
+    """Represent PL/SQL function."""
+
     name: str
+    args: list[Argument]
+    return_value: str | None
+
+
+@dataclass
+class Procedure:
+    """Represent PL/SQL procedure."""
+
+    name: str
+    args: list[Argument]
